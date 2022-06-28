301d1713a2c9ff6276e524dba3fd22ba79b65caa
mehrdad <mehrdad.fmousavi@gmail.com>
2022-06-28 12:11:38 +0430

    hamburget button fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {BsSearch} from "react-icons/bs"
 import {HiOutlineMenuAlt4} from "react-icons/hi"
+import {AiOutlineClose} from "react-icons/ai"
 
 const Navbar = () => {
+
+  const [nav, setNav] = useState(false)
+
+  const hamburgerHandler = () => {
+    setNav(!nav)
+  }
+
   return (
     <div className='flex justify-between w-full items-center h-20 px-4 absolute z-10'>
         <div className='cursor-pointer'>
             <h1>KATELL.</h1>
         </div>
 
-        <ul className='hidden sm:block sm:flex'>
+        <ul className='hidden md:block md:flex'>
             <li>خانه</li>
             <li>نمونه کارها</li>
             <li>فرایند ساخت</li>
             <li>ارتباط با ما</li>
             <li>درباره ما</li>
         </ul>
-
-        <div className='xs:hidden flex cursor-pointer'>
-            <HiOutlineMenuAlt4 size={30} />
+        
+        {/* Hamburger button */}
+        <div className='md:hidden flex cursor-pointer' onClick={hamburgerHandler}>
+            {
+              nav ? <HiOutlineMenuAlt4 size={30} /> : <AiOutlineClose size={30} />
+            }
+            
         </div>
 
     </div>
